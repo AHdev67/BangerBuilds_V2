86069afd4fced7292ad2de825a5db15d085f05bd
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-13 16:53:31 +0200

    added order creation method (does not support payment yet)

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,56 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Order;
+use App\Form\OrderType;
+use App\Repository\OrderRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
-    #[Route('/order', name: 'app_order')]
-    public function index(): Response
+    #[Route('/order/new', name: 'new_order')]
+    #[Route('/order/{orderId}/edit', name: 'edit_order')]
+
+    public function newOrder(Order $order = null, Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+        if (!$order){
+            $order = new Order();
+        }
+
+        $form = $this->createForm(OrderType::class, $order);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            $order->setRefId("69420");
+            $order = $form->getData();
+            $order->setOrderDate(new \DateTime());
+            $order->setTotal(0);
+
+            $session = $request->getSession();
+            $cart = $session->get('cart');
+            if ($cart) {
+                foreach ($cart as $item) {
+                    $product = $item['product'];
+                    $entityManager->persist($product);
+                    dd($product);
+                    $order->addProduct($product);
+                    $order->setTotal($order->getTotal() + $product->getPrice());
+                }   
+            }
+
+            $entityManager->persist($order);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('order/new_order.html.twig', [
+            'formOrder' => $form
         ]);
     }
 }
