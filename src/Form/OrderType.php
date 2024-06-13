3c52bfbc070d1239b97d2121a127c8f0d8f8dfa8
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-13 16:54:38 +0200

    added order form type

---
+++ src/Form/OrderType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Order;
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName')
+            ->add('firstName')
+            ->add('adress')
+            ->add('city')
+            ->add('postalCode')
+            
+            ->add('Validate', SubmitType::class, [
+                'attr' => [
+                    'class' => 'validateBtn submitBtn',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Order::class,
+        ]);
+    }
+}
