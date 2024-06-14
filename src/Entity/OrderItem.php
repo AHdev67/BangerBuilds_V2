141d6a5929c87c0f003ef7cbe3679c5209cf6f3b
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-14 17:12:29 +0200

    added orderItem entity

---
+++ src/Entity/OrderItem.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderItemRepository::class)]
+class OrderItem
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orderItems')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Order $relatedOrder = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orderItems')]
+    private ?Product $product = null;
+
+    #[ORM\ManyToOne(inversedBy: 'orderItems')]
+    private ?Build $build = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRelatedOrder(): ?Order
+    {
+        return $this->relatedOrder;
+    }
+
+    public function setRelatedOrder(?Order $relatedOrder): static
+    {
+        $this->relatedOrder = $relatedOrder;
+
+        return $this;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): static
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    public function getBuild(): ?Build
+    {
+        return $this->build;
+    }
+
+    public function setBuild(?Build $build): static
+    {
+        $this->build = $build;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+}
