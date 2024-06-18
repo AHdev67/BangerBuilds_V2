20c41bd011c24c903b4b008ac2dd73706850fb41
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-18 10:23:47 +0200

    adjusted order creation method to use the new orderItem entity

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use App\Entity\OrderItem;
 use App\Form\OrderType;
 use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
@@ -30,25 +31,26 @@ class OrderController extends AbstractController
             
             $order->setRefId("69420");
             $order = $form->getData();
+            $order->setUser($this->getUser());
             $order->setOrderDate(new \DateTime());
             $order->setTotal(0);
 
             $session = $request->getSession();
             $cart = $session->get('cart');
-            // if (!empty($cart)) {
-            //     foreach ($cart as $productId => $item) {
-            //         $product = $productRepository->find($productId);
-            //         for ($i = 0; $i < $item['qtt']; $i++) {
-            //             $order->addProduct($product);
-            //             $order->setTotal($order->getTotal() + $product->getPrice());
-            //         }
-            //     }   
-            // }
-
-            // REWORK METHOD TO USE NEW ORDER ITEM ENTITY !
-
+            if (!empty($cart)) {
+                foreach ($cart as $productId => $item) {
+                    $product = $productRepository->find($productId);
+                    $orderItem = new OrderItem;
+                    $orderItem->setProduct($product);
+                    $orderItem->setQuantity($item['qtt']);
+                    $entityManager->persist($orderItem);
+                    $order->addOrderItem($orderItem);
+                    $order->setTotal($order->getTotal() + $orderItem->getProduct()->getPrice() * $orderItem->getQuantity());
+                }   
+            }
             $entityManager->persist($order);
             $entityManager->flush();
+            $session->set('cart', []);
 
             return $this->redirectToRoute('app_user');
         }
