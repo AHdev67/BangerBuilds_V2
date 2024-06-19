f2c709f043daa84fbcc3c73e5e80a0d1d62fe51a
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-19 16:58:53 +0200

    added build form type

---
+++ src/Form/BuildType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Build;
+use App\Entity\Product;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BuildType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('total')
+            ->add('components', EntityType::class, [
+                'class' => Product::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Build::class,
+        ]);
+    }
+}
