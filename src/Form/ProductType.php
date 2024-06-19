a02fe2c6c5b6ce09e89801d5c2c26c50b212b286
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-19 16:59:02 +0200

    added product form type

---
+++ src/Form/ProductType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Build;
+use App\Entity\Product;
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label')
+            ->add('price')
+            ->add('specs')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+            ->add('image')
+
+            ->add('Validate', SubmitType::class, [
+                'attr' => [
+                    'class' => 'validateBtn submitBtn btn',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
