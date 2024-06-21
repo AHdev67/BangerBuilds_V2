b95e1ee7ecccad62e1dd884b364892b817af54be
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-21 16:59:20 +0200

    latest migration

--- migrations/Version20240620135513.php
+++ migrations/Version20240620135513.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240614145928 extends AbstractMigration
+final class Version20240620135513 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,8 @@ final class Version20240614145928 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE build (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, related_order_id INT DEFAULT NULL, total DOUBLE PRECISION NOT NULL, INDEX IDX_BDA0F2DBF675F31B (author_id), INDEX IDX_BDA0F2DB2B1C2395 (related_order_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE build_product (build_id INT NOT NULL, product_id INT NOT NULL, INDEX IDX_FEDF792117C13F8B (build_id), INDEX IDX_FEDF79214584665A (product_id), PRIMARY KEY(build_id, product_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE build (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, total DOUBLE PRECISION NOT NULL, INDEX IDX_BDA0F2DBF675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE build_component (id INT AUTO_INCREMENT NOT NULL, component_id INT NOT NULL, related_build_id INT DEFAULT NULL, quantity INT NOT NULL, INDEX IDX_F07C5604E2ABAFFF (component_id), INDEX IDX_F07C5604B1427126 (related_build_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, ref_id VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, adress VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, postal_code VARCHAR(50) NOT NULL, order_date DATETIME NOT NULL, total DOUBLE PRECISION NOT NULL, INDEX IDX_F5299398A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE order_item (id INT AUTO_INCREMENT NOT NULL, related_order_id INT NOT NULL, product_id INT DEFAULT NULL, build_id INT DEFAULT NULL, quantity INT NOT NULL, INDEX IDX_52EA1F092B1C2395 (related_order_id), INDEX IDX_52EA1F094584665A (product_id), INDEX IDX_52EA1F0917C13F8B (build_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -30,9 +30,8 @@ final class Version20240614145928 extends AbstractMigration
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE build ADD CONSTRAINT FK_BDA0F2DBF675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE build ADD CONSTRAINT FK_BDA0F2DB2B1C2395 FOREIGN KEY (related_order_id) REFERENCES `order` (id)');
-        $this->addSql('ALTER TABLE build_product ADD CONSTRAINT FK_FEDF792117C13F8B FOREIGN KEY (build_id) REFERENCES build (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE build_product ADD CONSTRAINT FK_FEDF79214584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE build_component ADD CONSTRAINT FK_F07C5604E2ABAFFF FOREIGN KEY (component_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE build_component ADD CONSTRAINT FK_F07C5604B1427126 FOREIGN KEY (related_build_id) REFERENCES build (id)');
         $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE order_item ADD CONSTRAINT FK_52EA1F092B1C2395 FOREIGN KEY (related_order_id) REFERENCES `order` (id)');
         $this->addSql('ALTER TABLE order_item ADD CONSTRAINT FK_52EA1F094584665A FOREIGN KEY (product_id) REFERENCES product (id)');
@@ -46,9 +45,8 @@ final class Version20240614145928 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE build DROP FOREIGN KEY FK_BDA0F2DBF675F31B');
-        $this->addSql('ALTER TABLE build DROP FOREIGN KEY FK_BDA0F2DB2B1C2395');
-        $this->addSql('ALTER TABLE build_product DROP FOREIGN KEY FK_FEDF792117C13F8B');
-        $this->addSql('ALTER TABLE build_product DROP FOREIGN KEY FK_FEDF79214584665A');
+        $this->addSql('ALTER TABLE build_component DROP FOREIGN KEY FK_F07C5604E2ABAFFF');
+        $this->addSql('ALTER TABLE build_component DROP FOREIGN KEY FK_F07C5604B1427126');
         $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395');
         $this->addSql('ALTER TABLE order_item DROP FOREIGN KEY FK_52EA1F092B1C2395');
         $this->addSql('ALTER TABLE order_item DROP FOREIGN KEY FK_52EA1F094584665A');
@@ -57,7 +55,7 @@ final class Version20240614145928 extends AbstractMigration
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6F675F31B');
         $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C67922C624');
         $this->addSql('DROP TABLE build');
-        $this->addSql('DROP TABLE build_product');
+        $this->addSql('DROP TABLE build_component');
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE `order`');
         $this->addSql('DROP TABLE order_item');
