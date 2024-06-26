f61eddbe61cc2cd7c937bed7d1bdb6f0413e59ec
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-26 08:39:45 +0200

    modified build formtype to use unmapped component slots for the configurator

--- src/Form/BuildType.php
+++ src/Form/BuildType.php
@@ -2,23 +2,33 @@
 
 namespace App\Form;
 
-use App\Entity\Build;
-use App\Entity\BuildComponent;
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Build;
+use App\Entity\Product;
+use App\Entity\BuildComponent;
+use App\Form\BuildComponentType;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class BuildType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder          
-            ->add('buildComponents', EntityType::class, [
-                'class' => BuildComponent::class,
-                'choice_label' => 'label'
-            ])
+        ->add('cpu', EntityType::class, [
+            'class' => Product::class,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 'cpu');
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Processors',
+        ])
         ;
     }
 
