efbb5699e93ff6a02fdf7a6807bb97bf461211a6
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-26 09:46:12 +0200

    added (wip) new/edit build method

--- src/Controller/BuildController.php
+++ src/Controller/BuildController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Build;
+use App\Form\BuildType;
+use App\Entity\BuildComponent;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BuildController extends AbstractController
 {
-    #[Route('/build', name: 'app_build')]
-    public function index(): Response
+
+
+    //  RETURNS THE CREATION FORM FOR A NEW BUILD / EDIT FORM FOR AN EXISITNG BUILD
+
+    #[Route('/build/new', name: 'new_build')]
+    public function newBuild(Build $build = null, Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('build/new_build.html.twig', [
-            'controller_name' => 'BuildController',
+        if (!$build) {
+            $build = new Build();
+        }
+
+        $form = $this->createForm(BuildType::class, $build);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $cpu = $form->get('cpu')->getData();
+
+            $buildComponentCpu = new BuildComponent();
+            $buildComponentCpu->setComponent($cpu);
+            $buildComponentCpu->setQuantity(1);
+            $build->addBuildComponent($buildComponentCpu);
+
+            $entityManager->persist($build);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('build/new.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
