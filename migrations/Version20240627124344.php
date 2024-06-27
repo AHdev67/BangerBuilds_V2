2b9715a88ca7e74f8e202db4a10d827c18193601
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-27 17:01:56 +0200

    latest migration

--- migrations/Version20240627124344.php
+++ migrations/Version20240627124344.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240624070329 extends AbstractMigration
+final class Version20240627124344 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,17 +21,18 @@ final class Version20240624070329 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE build (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, total DOUBLE PRECISION NOT NULL, INDEX IDX_BDA0F2DBF675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE build_component (id INT AUTO_INCREMENT NOT NULL, component_id INT NOT NULL, related_build_id INT DEFAULT NULL, quantity INT NOT NULL, INDEX IDX_F07C5604E2ABAFFF (component_id), INDEX IDX_F07C5604B1427126 (related_build_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE build_component (id INT AUTO_INCREMENT NOT NULL, component_id INT DEFAULT NULL, related_build_id INT DEFAULT NULL, category_id INT DEFAULT NULL, quantity INT NOT NULL, INDEX IDX_F07C5604E2ABAFFF (component_id), INDEX IDX_F07C5604B1427126 (related_build_id), INDEX IDX_F07C560412469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, ref_id VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, adress VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, postal_code VARCHAR(50) NOT NULL, order_date DATETIME NOT NULL, total DOUBLE PRECISION NOT NULL, INDEX IDX_F5299398A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE order_item (id INT AUTO_INCREMENT NOT NULL, related_order_id INT NOT NULL, product_id INT DEFAULT NULL, quantity INT NOT NULL, INDEX IDX_52EA1F092B1C2395 (related_order_id), INDEX IDX_52EA1F094584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, label VARCHAR(255) DEFAULT NULL, price DOUBLE PRECISION DEFAULT NULL, in_stock TINYINT(1) NOT NULL, restock_delay INT DEFAULT NULL, specs JSON NOT NULL, image VARCHAR(255) DEFAULT NULL, INDEX IDX_D34A04AD12469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, label VARCHAR(255) DEFAULT NULL, price DOUBLE PRECISION DEFAULT NULL, in_stock TINYINT(1) NOT NULL, restock_delay INT DEFAULT NULL, specs JSON NOT NULL, image VARCHAR(255) DEFAULT NULL, description LONGTEXT DEFAULT NULL, INDEX IDX_D34A04AD12469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, author_id INT NOT NULL, reviewed_product_id INT NOT NULL, title VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, review_date DATETIME NOT NULL, rating INT DEFAULT NULL, INDEX IDX_794381C6F675F31B (author_id), INDEX IDX_794381C67922C624 (reviewed_product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE build ADD CONSTRAINT FK_BDA0F2DBF675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE build_component ADD CONSTRAINT FK_F07C5604E2ABAFFF FOREIGN KEY (component_id) REFERENCES product (id)');
         $this->addSql('ALTER TABLE build_component ADD CONSTRAINT FK_F07C5604B1427126 FOREIGN KEY (related_build_id) REFERENCES build (id)');
+        $this->addSql('ALTER TABLE build_component ADD CONSTRAINT FK_F07C560412469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE order_item ADD CONSTRAINT FK_52EA1F092B1C2395 FOREIGN KEY (related_order_id) REFERENCES `order` (id)');
         $this->addSql('ALTER TABLE order_item ADD CONSTRAINT FK_52EA1F094584665A FOREIGN KEY (product_id) REFERENCES product (id)');
@@ -46,6 +47,7 @@ final class Version20240624070329 extends AbstractMigration
         $this->addSql('ALTER TABLE build DROP FOREIGN KEY FK_BDA0F2DBF675F31B');
         $this->addSql('ALTER TABLE build_component DROP FOREIGN KEY FK_F07C5604E2ABAFFF');
         $this->addSql('ALTER TABLE build_component DROP FOREIGN KEY FK_F07C5604B1427126');
+        $this->addSql('ALTER TABLE build_component DROP FOREIGN KEY FK_F07C560412469DE2');
         $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395');
         $this->addSql('ALTER TABLE order_item DROP FOREIGN KEY FK_52EA1F092B1C2395');
         $this->addSql('ALTER TABLE order_item DROP FOREIGN KEY FK_52EA1F094584665A');
