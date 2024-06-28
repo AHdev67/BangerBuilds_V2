96d7b5bd2cf688b104b55dfa114b01f88cee551a
AHdev67 <antoinehelfer.contact@gmail.com>
2024-06-28 13:29:56 +0200

    added unmapped fields for each component slots

--- src/Form/BuildType.php
+++ src/Form/BuildType.php
@@ -8,6 +8,8 @@ use App\Entity\Product;
 use App\Entity\BuildComponent;
 use App\Form\BuildComponentType;
 use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -21,16 +23,208 @@ class BuildType extends AbstractType
     {
 
         $builder          
-        ->add('buildComponents', CollectionType::class, [
-            'entry_type' => BuildComponentType::class,
-            'entry_options' => [
-                'label' => false,
-                // 'category' => 'category'
-            ],
-            'by_reference' => false,
-            'allow_add' => true,
-            'allow_delete' => true,
-            'prototype' => true,
+        // ->add('buildComponents', CollectionType::class, [
+        //     'entry_type' => BuildComponentType::class,
+        //     'entry_options' => function (FormBuilderInterface $builder) {
+        //         $data = $builder->getData();
+        //         $category = $data->getCategory() ? $data->getCategory()->getId() : null;
+
+        //         return [
+        //             'label' => false,
+        //             'category' => $category,
+        //         ];
+        //     },
+        //     'by_reference' => false,
+        //     'allow_add' => true,
+        //     'allow_delete' => true,
+        //     'prototype' => true,
+        // ])
+
+        ->add('cpu', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 1);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('cpuCooler', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 2);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('motherboard', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 3);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('memory', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 4);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('graphicsCard', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 5);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('storage', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 6);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('powerSupply', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 9);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('case', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 8);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('opticalDrive', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'required' =>false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 10);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('wiredNetworkCard', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'required' =>false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 11);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('wirelessNetworkCard', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'required' =>false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 12);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('soundCard', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'required' =>false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 13);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('operatingSystem', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'required' =>false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 14);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('caseFan', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'required' =>false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 16);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
+        ])
+
+        ->add('service', EntityType::class, [
+            'class' => Product::class,
+            'mapped' => false,
+            'required' =>false,
+            'query_builder' => function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('p')
+                    ->where('p.category = :category')
+                    ->setParameter('category', 26);
+            },
+            'choice_label' => 'label',
+            'placeholder' => 'Choose a product',
         ])
 
         ->add('save', SubmitType::class, [
@@ -39,11 +233,10 @@ class BuildType extends AbstractType
         ])
         ;
     }
-
+    
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Build::class,
         ]);
     }
 }
