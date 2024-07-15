de299e6234be5e8047413345d823a4295ac59382
AHdev67 <antoinehelfer.contact@gmail.com>
2024-07-15 15:51:16 +0200

    added attempt at a json response method to process JS query

--- src/Controller/BuildController.php
+++ src/Controller/BuildController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Build;
+use App\Entity\Product;
 use App\Form\BuildType;
 use App\Entity\Category;
 use App\Entity\BuildComponent;
+use App\Form\ComponentSlotType;
 use App\Repository\BuildRepository;
 use App\Repository\ProductRepository;
 use App\Repository\CategoryRepository;
@@ -15,6 +17,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BuildController extends AbstractController
@@ -52,13 +55,52 @@ class BuildController extends AbstractController
     public function newBuild(Build $build = null ,Request $request, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository, ProductRepository $productRepository): Response
     {
 
-        //  BUILD OBJECT INITIALIZATION
+//---------------------------------------------------------------------------------------------------------------------------------------
+        // if(!$build){
+        //     $build = new Build();
+        //     $build->setTotal(0);
 
+        //     $categories = [
+        //         $categoryRepository->findOneBy(['name' => "Processor"]),
+        //         $categoryRepository->findOneBy(['name' => "CPU cooler"]),
+        //         $categoryRepository->findOneBy(['name' => "Motherboard"]),
+        //         $categoryRepository->findOneBy(['name' => "Memory"]),
+        //     ];
+            
+        //     foreach ($categories as $category) {
+        //         $component = new BuildComponent();
+        //         $component->setCategory($category);
+        //         $build->addBuildComponent($component);
+        //     }
+
+        //     } else {
+        //         $categories = [];
+        //         foreach ($build->getBuildComponents() as $buildComponent) {
+        //             $categories[] = $buildComponent->getCategory();
+        //         }
+        //     }
+
+        //     // Fetch products based on categories
+        //     $productsByCategory = [];
+        //     foreach ($categories as $category) {
+        //     $productsByCategory[$category->getId()] = $productRepository->findBy(['category' => $category]);
+        // }
+
+        // $form = $this->createForm(BuildType::class, $build, ['products_by_category' => $productsByCategory]);
+
+        // $form->handleRequest($request);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $build = $form->getData();
+        // }
+//---------------------------------------------------------------------------------------------------------------------------------------
+       
         if(!$build){
             $build = new Build();
-            $build->setTotal(0); 
+            $build->setTotal(0);        
+
         }
-       
+
         $form = $this->createForm(BuildType::class, $build);
 
         $form->handleRequest($request);
@@ -93,7 +135,7 @@ class BuildController extends AbstractController
                     $build->addToTotal($buildComponent->getComponent()->getPrice() * $buildComponent->getQuantity());
                 }
             }
-
+//---------------------------------------------------------------------------------------------------------------------------------------
             $build->setAuthor($this->getUser());
             
             $entityManager->persist($build);
@@ -116,5 +158,26 @@ class BuildController extends AbstractController
         $entityManager->flush();
 
         return $this->redirectToRoute('app_user');
+    }
+
+
+    public function getMotherboards(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $cpuId = $request->query->get('cpuId');
+        $cpu = $this->$entityManager->getRepository(Product::class)->find($cpuId);
+
+        $motherboards = [];
+
+        if ($cpu && strpos($cpu->getLabel(), 'Intel') !== false) {
+            $motherboards = $this->$entityManager->getRepository(Product::class)->createQueryBuilder('p')
+                ->where('p.category = :category')
+                ->andWhere('p.label LIKE :pattern')
+                ->setParameter('category', 2)
+                ->setParameter('pattern', 'Z%')
+                ->getQuery()
+                ->getResult();
+        }
+
+        return $this->json($motherboards);
     }
 }
