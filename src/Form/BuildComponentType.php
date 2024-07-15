6a467406206cd32cb5d5e0c19421ce5a38482c78
AHdev67 <antoinehelfer.contact@gmail.com>
2024-07-15 15:52:17 +0200

    attempted to retrieve options from collectionType, did not work

--- src/Form/BuildComponentType.php
+++ src/Form/BuildComponentType.php
@@ -7,6 +7,7 @@ use App\Entity\Product;
 use App\Entity\Category;
 use App\Entity\BuildComponent;
 use Doctrine\ORM\EntityRepository;
+use App\Repository\ProductRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
@@ -14,23 +15,29 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BuildComponentType extends AbstractType
 {
+    
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $category = $options['category'] ?? null;
+        $productsByCategory = $options['products_by_category'] ?? [];
 
-        $category = $options['category'];
+        // Debugging output
+        dump($category);
+        dump($productsByCategory);
+
+        $products = $category ? ($productsByCategory[$category->getId()] ?? []) : [];
+
+        // Debugging output
+        dump($products);
 
         $builder
-            ->add('component', EntityType::class, [
+            ->add('component', EntityType::class, [ 
                 'class' => Product::class,
-                'query_builder' => function (EntityRepository $entityRepository) use ($category) {
-                    return $entityRepository->createQueryBuilder('p')
-                        ->where('p.category = :category')
-                        ->setParameter('category', $category);
-                },
-                'choice_label' => 'label',
+                'choices' => $products,
+                'choice_label' => 'name',
                 'placeholder' => 'Choose a product',
-            ])
-        ;
+                'label' => false,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -38,7 +45,10 @@ class BuildComponentType extends AbstractType
         $resolver->setDefaults([
             'data_class' => BuildComponent::class,
             'category' => null,
+            'products_by_category' => [],
         ]);
-        $resolver->setAllowedTypes('category', ['null', 'int']);
+
+        $resolver->setAllowedTypes('category', ['null', 'App\Entity\Category']);
+        $resolver->setAllowedTypes('products_by_category', 'array');
     }
 }
