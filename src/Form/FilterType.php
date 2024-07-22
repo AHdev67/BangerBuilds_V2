981f85ace28e3a517efee43bd081ec73d52f667c
AHdev67 <antoinehelfer.contact@gmail.com>
2024-07-22 12:01:42 +0200

    added formtype to handle filters on product list view

---
+++ src/Form/FilterType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use EventListener\AddBrandFieldSubscriber;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class FilterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $category = $options['category'] ?? [];
+
+        $builder
+            ->add('orderBy', ChoiceType::class, [
+                'label' => 'Order products by :',
+                'choices' => [
+                    'Highest price' => 'price_ASC',
+                    'Lowest price' => 'price_DESC',
+                    'Manufacturer' => 'brand_ASC',
+                    'Score' => 'score_ASC',
+                ],
+            ])
+
+            ->addEventSubscriber(new AddBrandFieldSubscriber($category))
+            
+            ->add('apply', SubmitType::class, [
+                'label' => 'Apply filters',
+                'attr' => ['class' => 'validateBtn btn'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'category' => [],
+        ]);
+
+        $resolver->setAllowedTypes('category', Category::class);
+    }
+}
