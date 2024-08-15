3960cfd9ff922f4593917c53411f2719935e50e6
AHdev67 <antoinehelfer.contact@gmail.com>
2024-08-15 03:01:24 +0200

    now properly fetches trending products

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,18 +20,15 @@ class HomeController extends AbstractController
         $popularCategories = [];
             $popularCategories["categ1"] = $categoryRepository->findOneBy(["name"=>"Processor"]);
             $popularCategories["categ2"] = $categoryRepository->findOneBy(["name"=>"Video card"]);
-            $popularCategories["categ3"] = $categoryRepository->findOneBy(["name"=>"Service"]);
+            $popularCategories["categ3"] = $categoryRepository->findOneBy(["name"=>"Memory"]);
+            $popularCategories["categ4"] = $categoryRepository->findOneBy(["name"=>"Service"]);
 
-        $newestProducts = [];
-            $newestProducts["prod1"] = $productRepository->findOneBy(["label"=>"AMD Ryzen 7 7800X3D"]);
-            $newestProducts["prod2"] = $productRepository->findOneBy(["label"=>"Gigabyte WINDFORCE OC GeForce RTX 4070 Ti SUPER"]);
-            $newestProducts["prod3"] = $productRepository->findOneBy(["label"=>"Sapphire PULSE Radeon RX 7900 XT"]);
-            $newestProducts["prod4"] = $productRepository->findOneBy(["label"=>"Lian Li O11 Dynamic EVO"]);
-            $newestProducts["prod5"] = $productRepository->findOneBy(["label"=>"MSI B760 GAMING PLUS WIFI"]);
+        $trendingProducts = [];
+            $trendingProducts = $productRepository->findTop5Products();
 
         return $this->render('home/homepage.html.twig', [
             'popularCategories' => $popularCategories,
-            'newestProducts' => $newestProducts,
+            'trendingProducts' => $trendingProducts,
         ]);
     }
 }
